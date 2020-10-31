3374883a95111253dcb825adb11805857a5ae789
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2020-10-31 17:52:22 +0400

    Create yan_sentence_segmentation_example.py

---
+++ yan_sentence_segmentation_example.py
@@ -0,0 +1,5 @@
+######yan_sentence_segmentation_example.py###
+from yan_sentence_segmentation import text_to_sentences
+
+print(text_to_sentences("Barack Obama was born in Hawaii.  He was elected president in 2008."))
+######yan_sentence_segmentation_example.py###
