2afebfb01408247f3f1fd691b83a078a18794668
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2020-11-15 14:35:21 +0400

    Create yan_ner_arabic_example.py

---
+++ yan_ner_arabic_example.py
@@ -0,0 +1,7 @@
+##########yan_ner_arabic_example.py###
+from yan_ner_arabic import arabic_ner
+
+text = u"اسمي مشاري. انا اعمل في جامعة حائل. انا سعودي."
+
+print(arabic_ner(text))
+##########yan_ner_arabic_example.py###
