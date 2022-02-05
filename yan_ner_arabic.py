7af309c388ab5e65d88db6972f4e011b39ecf4bf
Yan Liang <yanliang2345@outlook.com>
2022-02-05 08:57:06 +0400

    Add files via upload

--- yan_ner_arabic.py
+++ yan_ner_arabic.py
@@ -1,4 +1,9 @@
 ##########yan_ner_arabic.py###########
+
+'''
+https://universaldependencies.org/treebanks/ar_padt/index.html
+'''
+
 import stanza
 
 nlp = stanza.Pipeline('ar')
@@ -9,4 +14,17 @@ def arabic_ner(text):
 		return [e.to_dict() for e in doc.ents]
 	except:
 		return None
+
+
+def text_to_lemma_text(
+	text,
+	):
+	try:
+		doc = nlp(text)
+		lemma_text = ' '.join([w.lemma for w in doc.iter_words()])
+		return lemma_text
+	except:
+		return None
+
+
 ##########yan_ner_arabic.py###########
